b9341321b9aab56dfa1c27100e2b822531a8d7c1
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-15 10:24:31 +0530

    initial

--- app/page.js
+++ app/page.js
@@ -2,9 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='px-[2rem] bg-blue-600 min-h-screen'>
-    <div className='bg-red-800'>Map</div>
-    <div className='bg-green-800'>Start</div>
+    <div className='flex h-screen'>
+    <div className='flex-1 bg-red-800 text-gray-800 flex items-center justify-center'>
+      Map
     </div>
-  )
+    <div className='flex-1 bg-red-300 text-gray-800 flex items-center justify-center'>
+      Start
+    </div>
+  </div>
+  
+  );
 }
