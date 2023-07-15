5d8c5f9018fbbe86b406b2b444e9fd98a2bf460c
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-15 16:51:05 +0530

    framer motion added

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
 <div align='center'>
+
 <h2>Create a ride-sharing app that focuses on empowering and supporting marginalized genders in accessing safe, reliable, and affordable transportation options. The app aims to provide a platform where women and marginalized genders can feel confident and secure when using transportation services, whether as drivers or passengers.</h2>
 <h1>Features</h1>
 <p>Features:
 
-Safe Ride Options: Implement safety features such as SOS alerts, real-time tracking, and emergency contacts to ensure the safety of passengers and drivers.
+1. Safe Ride Options: Implement safety features such as SOS alerts, real-time tracking, and emergency contacts to ensure the safety of passengers and drivers.
 
-Gender-Inclusive Registration: Provide gender-inclusive registration options to ensure inclusivity and respect for all users.
+2. Gender-Inclusive Registration: Provide gender-inclusive registration options to ensure inclusivity and respect for all users.
 
-Women/Marginalized Gender Drivers: Encourage and support women and marginalized genders to become drivers, providing opportunities for financial independence and flexible work hours.
+3. Women/Marginalized Gender Drivers: Encourage and support women and marginalized genders to become drivers, providing opportunities for financial independence and flexible work hours.
 
-Driver Verification: Implement a thorough driver verification process to ensure the trustworthiness and reliability of drivers.
+4. Driver Verification: Implement a thorough driver verification process to ensure the trustworthiness and reliability of drivers.
 
-Safe Zones and Preferred Routes: Allow users to mark and share safe zones or preferred routes, which can help other users make informed choices.
+5. Safe Zones and Preferred Routes: Allow users to mark and share safe zones or preferred routes, which can help other users make informed choices.
 
-Feedback and Rating System: Implement a feedback and rating system to create a supportive community and to ensure accountability and improvement in service quality.
+6. Feedback and Rating System: Implement a feedback and rating system to create a supportive community and to ensure accountability and improvement in service quality.
 
-Financial Empowerment: Offer competitive pricing for rides and ensure transparent fare calculations. Consider offering incentives and bonuses for women/marginalized gender drivers to encourage more participation.
+7. Financial Empowerment: Offer competitive pricing for rides and ensure transparent fare calculations. Consider offering incentives and bonuses for women/marginalized gender drivers to encourage more participation.
 
-Community Support: Create a community support system where users can share experiences, tips, and resources related to transportation safety.
+8. Community Support: Create a community support system where users can share experiences, tips, and resources related to transportation safety.
 
-Accessibility: Ensure the app is accessible and user-friendly to accommodate a diverse range of users.</p>
+9. Accessibility: Ensure the app is accessible and user-friendly to accommodate a diverse range of users.
+    
+    
+</p>
 
 </div>
--- src/components/Driver/DriverHome.js
+++ src/components/Driver/DriverHome.js
@@ -4,20 +4,22 @@ import { Link } from 'react-router-dom';
 import { FaMapMarkerAlt, FaCar } from 'react-icons/fa';
 import DriverHeader from './DriverHeader';
 import mapboxgl from 'mapbox-gl';
+import { motion } from 'framer-motion';
 
 const DriverHome = () => {
   const { isAuthenticated, user } = useAuth0();
   const mapContainerRef = useRef(null);
+  const mapRef = useRef(null);
+
   useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoidmFpc2huYXZpMzk2OSIsImEiOiJjbGpiOGhqd2UxdGdyM2hxbm1vMDZxa2JqIn0.nsJYS6QQxVmEr2ZajJywYQ';
-
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40],
+      center: [-74.5, 64],
       zoom: 9,
     });
-
+    mapRef.current = map;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
@@ -27,28 +29,55 @@ const DriverHome = () => {
     }
   }, []);
 
+  const handleResetLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(mapRef.current);
+        mapRef.current.setCenter([longitude, latitude]);
+      });
+    }
+  };
   return (
-    <div className="bg-gray-900 min-h-screen">
-      <DriverHeader />
-      <div className="container mx-auto py-8">
-        {isAuthenticated && (
-          <div className="text-right">
-            <p className="text-white">Welcome, {user.name}!</p>
-            <p className="text-white">Email: {user.email}</p>
-          </div>
-        )}
+    <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    className="bg-gray-900 min-h-screen"
+  >
+    <DriverHeader />
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      className="container mx-auto py-8"
+    >
+      {isAuthenticated && (
+        <motion.div
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          className="text-right"
+        >
+          <p className="text-white">Welcome, {user.name}!</p>
+          <p className="text-white">Email: {user.email}</p>
+        </motion.div>
+      )}
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-12 md:col-span-6 lg:col-span-3 p-4 bg-gray-800 rounded-lg">
             <div className="flex items-center justify-center mb-4">
               <FaMapMarkerAlt size={36} className="text-blue-500 mr-2" />
               <h2 className="text-lg font-semibold text-white">Current Location</h2>
             </div>
-            <div
-              ref={mapContainerRef}
-              className="h-40 bg-white rounded-lg"
-              style={{ width: '100%' }}
-            />
-
+            <div className="h-64 bg-white rounded-lg overflow-hidden" style={{ width: '100%' }}>
+              <div ref={mapContainerRef} style={{ width: '100%', height: '100%' }} />
+            </div>
+            <div className="mt-2 flex justify-center">
+              <button
+                className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded-md shadow-md"
+                onClick={handleResetLocation}
+              >
+                Reset Location
+              </button>
+            </div>
           </div>
 
           <div className="col-span-12 md:col-span-6 lg:col-span-3 p-4 bg-gray-800 rounded-lg">
@@ -56,8 +85,8 @@ const DriverHome = () => {
               <FaCar size={36} className="text-yellow-500 mr-2" />
               <h2 className="text-lg font-semibold text-white">Total Rides</h2>
             </div>
-            <div className="h-40 bg-white rounded-lg" >
-              <div className="h-40  flex items-center justify-center text-4xl font-bold text-gray-800">
+            <div className="h-64 bg-white rounded-lg" >
+              <div className="h-64  flex items-center justify-center text-4xl font-bold text-gray-800">
                 {Math.round(Math.random() * 100)}
               </div>
             </div>
@@ -68,8 +97,8 @@ const DriverHome = () => {
               <FaCar size={36} className="text-green-500 mr-2" />
               <h2 className="text-lg font-semibold text-white">Completed Rides</h2>
             </div>
-            <div className="h-40 bg-white rounded-lg" >
-              <div className="h-40  flex items-center justify-center text-4xl font-bold text-gray-800">
+            <div className="h-64 bg-white rounded-lg" >
+              <div className="h-64  flex items-center justify-center text-4xl font-bold text-gray-800">
                 {Math.round(Math.random() * 100)}
               </div>
             </div>
@@ -80,34 +109,50 @@ const DriverHome = () => {
               <FaCar size={36} className="text-red-500 mr-2" />
               <h2 className="text-lg font-semibold text-white">Cancelled Rides</h2>
             </div>
-            <div className="h-40 bg-white rounded-lg" >
-              <div className="h-40  flex items-center justify-center text-4xl font-bold text-gray-800">
+            <div className="h-64 bg-white rounded-lg" >
+              <div className="h-64  flex items-center justify-center text-4xl font-bold text-gray-800">
                 {Math.round(Math.random() * 100)}
               </div>
             </div>
           </div>
         </div>
 
-        <div className="mt-8">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="mt-8"
+        >
           <h1 className="text-4xl font-bold text-center mb-6 text-yellow-600">Driver Dashboard</h1>
           <p className="text-gray-100 text-center mb-8">
             Here, you can manage your rides and view your driver profile.
           </p>
-          <div className="flex justify-center space-x-4">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            className="flex justify-center space-x-4"
+          >
             <Link to="/driver-rides">
-              <button className="px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md shadow-md">
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                className="px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md shadow-md"
+              >
                 Manage Rides
-              </button>
+              </motion.button>
             </Link>
             <Link to="/driver-profile">
-              <button className="px-6 py-2 bg-green-500 hover:bg-green-600 text-white rounded-md shadow-md">
+              <motion.button
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                className="px-6 py-2 bg-green-500 hover:bg-green-600 text-white rounded-md shadow-md"
+              >
                 View Profile
-              </button>
+              </motion.button>
             </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
