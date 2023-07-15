8e92fb3232286c0bbdd94f59d68d6ccb03c9e625
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-15 10:06:40 +0530

    app initialized

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='px-[2rem] bg-blue-600 min-h-screen'>
+    <div className='bg-red-800'>Map</div>
+    <div className='bg-green-800'>Start</div>
+    </div>
+  )
+}
