44966efc1a70026d5e3e73eb7052d1949377b9e5
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-16 18:19:36 +0530

    conflicts updated

--- src/components/Rider/RiderNotifications.js
+++ src/components/Rider/RiderNotifications.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import RiderHeader from './RiderHeader'
-import { now } from 'mongoose'
 
 const data = [
   {message: "Your package has been delivered successfully.", time: "3 hours ago"},
@@ -16,32 +15,18 @@ const data = [
 ]
 // const [date] = new Date(now)
 const NotificationCard = (props) => {
- 
   return (
     <div className='bg-white text-gray-900 w-[80%] p-2 rounded-lg my-4'>
       <h1 className='text-xl'>{props.message}</h1>
       <p className='text-sm'>• {props.time} •</p>
     </div>
-  )
-}
+  );
+};
 
 const RiderNotifications = () => {
-  return (
-    <div>
-      <RiderHeader/>
-      <div className='bg-gray-900 p-24'>
-      <h1 className='text-white font-bold text-7xl mb-2'>Notifications</h1>
-      <p className='text-white  text-3xl my-2'>Recent</p>
-        <NotificationCard message="Your driver is on their way. They should reach in 10 minutes." time="Just Now"/>
-       
-      <p className='text-white  text-3xl my-4'>Older Notifications</p>
-      {data.map((details)=> {
-          return  <NotificationCard message={details.message} time={details.time}  />
-        })}
-      </div>
-    </div>
-  )
-}
+  const [notifications, setNotifications] = useState([]); 
+  const [showPopup, setShowPopup] = useState(false); 
+  const [selectedNotification, setSelectedNotification] = useState(null); 
 
   const markAsRead = (index) => {
     setNotifications((prevNotifications) => {
@@ -66,77 +51,27 @@ const RiderNotifications = () => {
           isRead: false,
         };
         setNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
-        playNotificationSound();
-      }, 3000); 
+        // playNotificationSound(); // Assuming you have a function to play the notification sound
+      }, 3000);
     };
     addNewNotification();
-  }, [playNotificationSound]);
+  }, []);
 
   return (
-    <div className="bg-gray-900 min-h-screen justify-center items-center">
-    <RiderHeader/>
-      <div className="max-w-md p-4 mx-auto">
-        <div className="flex items-center mb-4">
-          <FaBell className="mr-2 text-blue-500" size={24} />
-          <h2 className="text-lg font-semibold text-blue-500">Notifications</h2>
-        </div>
-        <AnimatePresence>
-          {notifications.map((notification, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, y: -20, scale: 0.8 }}
-              animate={{ opacity: 1, y: 0, scale: 1 }}
-              exit={{ opacity: 0, y: 20, scale: 0.8 }}
-              transition={{ duration: 0.3, type: 'spring' }}
-              className={`p-4 rounded-lg mb-2 ${
-                notification.isRead ? 'bg-gray-800' : 'bg-blue-500'
-              }`}
-              onClick={() => handleNotificationClick(index)}
-            >
-              <p className={`text-sm ${notification.isRead ? 'text-gray-300' : 'text-white'}`}>
-                {notification.message}
-              </p>
-              <p className="text-xs text-gray-500">
-                {new Date(notification.timestamp).toLocaleString()}
-              </p>
-              {!notification.isRead && (
-                <button
-                  className="text-xs text-blue-200 mt-2 focus:outline-none"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    markAsRead(index);
-                  }}
-                >
-                  Mark as Read
-                </button>
-              )}
-            </motion.div>
-          ))}
-        </AnimatePresence>
-        {notifications.length === 0 && (
-          <p className="text-center text-sm text-gray-500">No notifications</p>
-        )}
+    <div>
+      <RiderHeader />
+      <div className='bg-gray-900 p-24'>
+        <h1 className='text-white font-bold text-7xl mb-2'>Notifications</h1>
+        <p className='text-white text-3xl my-2'>Recent</p>
+        <NotificationCard message="Your driver is on their way. They should reach in 10 minutes." time="Just Now" />
+
+        <p className='text-white text-3xl my-4'>Older Notifications</p>
+        {data.map((details) => {
+          return <NotificationCard message={details.message} time={details.time} />;
+        })}
       </div>
-      {showPopup && (
-        <div
-          className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50"
-          onClick={() => setShowPopup(false)}
-        >
-          <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.5 }}
-            className="bg-white p-4 rounded-lg"
-          >
-            <h3 className="text-lg font-semibold mb-2">{selectedNotification?.message}</h3>
-            <p className="text-sm text-gray-500">
-              {selectedNotification && new Date(selectedNotification.timestamp).toLocaleString()}
-            </p>
-          </motion.div>
-        </div>
-      )}
     </div>
   );
-
+};
 
 export default RiderNotifications;
--- src/components/Rider/RiderRideHistory.js
+++ src/components/Rider/RiderRideHistory.js
@@ -1,22 +1,88 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import RiderHeader from './RiderHeader';
 
 const RiderRideHistory = () => {
-  return (
-    <div className='bg-gray-900'>
-      <RiderHeader/>
-      <div className='bg-gray-900 text-white p-24 flex items-center mx-auto justify-between'>
-        <h1 className='text-4xl font-bold'>Your Ride History</h1>
-        <Link to="/rider-request-form"><button className='bg-white p-4 text-gray-900 rounded-lg font-bold'>Request a New Ride</button></Link>
-      </div>
-      <div className='pb-24 px-24 flex flex-wrap gap-10 justify-center'>
-      {data.map((details)=>{
-        return <RideCard amount={details.amount} origin={details.origin} destination={details.destination} payment={details.payment}/>})}
-        
-      </div>
-        
-      
-    </div>
-  )
-}
+  const rideHistoryData = [
+    {
+      id: 1,
+      date: '2023-07-10',
+      from: 'Home',
+      to: 'Work',
+      distance: '10 km',
+      fare: '$12.50',
+      driver: 'John Doe',
+    },
+    {
+      id: 2,
+      date: '2023-07-12',
+      from: 'Work',
+      to: 'Home',
+      distance: '10 km',
+      fare: '$15.00',
+      driver: 'Jane Smith',
+    },
+    {
+      id: 3,
+      date: '2023-07-14',
+      from: 'Home',
+      to: 'Work',
+      distance: '10 km',
+      fare: '$12.50',
+      driver: 'John Doe',
+    },
+    {
+      id: 4,
+      date: '2023-07-16',
+      from: 'Work',
+      to: 'Home',
+      distance: '10 km',
+      fare: '$15.00',
+      driver: 'Jane Smith',
+    },
+      ];
 
-export default RiderPayments
+  return (
+    <div className="bg-gray-900 min-h-screen">
+    <RiderHeader/>
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-semibold text-white mb-4">Ride History</h2>
+        <motion.div
+          className="bg-white dark:bg-gray-800 shadow overflow-hidden sm:rounded-md"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <ul className="divide-y divide-gray-200">
+            {rideHistoryData.map((ride) => (
+              <li key={ride.id} className="hover:bg-gray-100 dark:hover:bg-gray-700">
+                <div className="px-4 py-4 sm:px-6">
+                  <div className="flex items-center justify-between">
+                    <p className="text-lg font-medium text-blue-600">{ride.date}</p>
+                    <p className="text-sm font-medium text-gray-100">{ride.distance}</p>
+                  </div>
+                  <div className="mt-2 sm:flex sm:justify-between">
+                    <div className="sm:flex text-gray-200">
+                      <p className="mr-4 mb-2 sm:mb-0">
+                        <span className="font-semibold">From:</span> {ride.from}
+                      </p>
+                      <p>
+                        <span className="font-semibold">To:</span> {ride.to}
+                      </p>
+                    </div>
+                    <div className="mt-2 sm:mt-0">
+                      <p className="text-sm font-medium text-gray-100">{ride.driver}</p>
+                      <p className="text-sm font-medium text-green-600">{ride.fare}</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default RiderRideHistory;
