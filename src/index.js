cb5f699e984ae83e4f4d45c7c1a9e0bf90ece89b
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-07-16 17:28:20 +0530

    updated auth0

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
-    domain="dev-2mgbgxm3b0scb3xq.us.auth0.com"
-    clientId="ygFFa9zzCPQPAqsABx1QAy0VP7Vwpqjb"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider>,
+  domain="dev-yny6k08ga11bopwa.us.auth0.com"
+  clientId="bVymAXNehb078MqiniQCg7ykEVzlQXZc"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+>
+  <App />
+</Auth0Provider>,
 );
 
