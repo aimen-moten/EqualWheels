b0d02f84c7b9123c38b70d00a2b1009ff723d516
Aimen <aimen.moten@gmail.com>
2023-07-16 17:14:28 +0500

    rider updates

--- src/components/Rider/RideRequestPage.js
+++ src/components/Rider/RideRequestPage.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import RiderHeader from './RiderHeader'
 import RideRequestForm from './RideRequestForm'
+import { Link } from 'react-router-dom'
+
 
 function RideRequestPage() {
   return (
     <div className='bg-gray-900'>
         <RiderHeader />
-        <div className='font-bold text-white text-center justify-center items-center p-8'>
+        <div className='font-bold text-white text-center justify-center items-center pt-24'>
           <h1 className='text-6xl'>Request a Ride</h1>
           <p className='text-3xl'>Safe and Affordable, Only for You!</p>
         </div>
-        <div>
+        <div className='p-16'>
             <RideRequestForm />
         </div>
         
